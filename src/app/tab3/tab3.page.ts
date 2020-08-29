8786b800804ad331262b111a515ab018f2d3bd4c
Benjamin Reed <ranger@opennms.org>
2020-08-29 12:01:05 -0400

    show version info in the about page

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { VERSION } from '../../environments/version';
 
 @Component({
   selector: 'app-tab3',
@@ -6,6 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  public VERSION = VERSION;
 
   constructor() {}
 
