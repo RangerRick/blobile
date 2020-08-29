4b8307643221b27560bea4a7b9a92678d311c0b2
Benjamin Reed <ranger@opennms.org>
2020-08-28 23:54:11 -0400

    automatic reload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { AlertController, IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { Deploy } from 'cordova-plugin-ionic/dist/ngx';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
@@ -20,4 +21,43 @@ import { AppComponent } from './app.component';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private alertController: AlertController, private deploy: Deploy) {
+  }
+
+  async doUpdate() {
+    try {
+      const currentVersion = await this.deploy.getCurrentVersion();
+      const resp = await this.deploy.sync({updateMethod: 'background'}, percentDone => {
+        console.debug(`Update is ${percentDone}% done!`);
+      });
+      if (!currentVersion || currentVersion.versionId !== resp.versionId) {
+        // We found an update, as if they want to update!
+        const alert = await this.alertController.create({
+          // cssClass: 'my-custom-class',
+          header: 'Update Available',
+          // subHeader: 'Subtitle',
+          message: 'An update is available. Apply now?',
+          buttons: [
+            {
+              text: 'Skip',
+              role: 'cancel',
+              cssClass: 'secondary',
+            },
+            {
+              text: 'Apply',
+              handler: () => {
+                this.deploy.reloadApp();
+              }
+            }
+          ]
+        });
+        await alert.present();
+      } else {
+        // No update available
+      }
+    } catch (err) {
+      console.error('Something went wrong attempting to update:', err);
+    }
+  }
+}
