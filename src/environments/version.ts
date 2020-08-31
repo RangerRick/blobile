dfda54798417ee1ec585b136081cb32d69d654ea
Benjamin Reed <ranger@opennms.org>
2020-08-31 17:21:42 -0400

    1.0.4 build 14

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -2,9 +2,9 @@
 /* tslint:disable */
 export const VERSION = {
     "dirty": true,
-    "raw": "v1.0.2-12-1-g96c10ea-dirty",
-    "hash": "g96c10ea",
-    "distance": 1,
+    "raw": "v1.0.2-12-7-g7353196-dirty",
+    "hash": "g7353196",
+    "distance": 7,
     "tag": "v1.0.2-12",
     "semver": {
         "options": {
@@ -22,9 +22,9 @@ export const VERSION = {
         "build": [],
         "version": "1.0.2-12"
     },
-    "suffix": "1-g96c10ea-dirty",
-    "semverString": "1.0.2-12+1.g96c10ea",
-    "version": "1.0.3",
-    "build": 13
+    "suffix": "7-g7353196-dirty",
+    "semverString": "1.0.2-12+7.g7353196",
+    "version": "1.0.4",
+    "build": 14
 };
 /* tslint:enable */
