e5274c75fafce2197f66f56849d06cef5767b39e
Benjamin Reed <ranger@opennms.org>
2020-09-02 12:09:29 -0400

    1.0.5 build 15

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -2,29 +2,29 @@
 /* tslint:disable */
 export const VERSION = {
     "dirty": true,
-    "raw": "v1.0.2-12-7-g7353196-dirty",
-    "hash": "g7353196",
-    "distance": 7,
-    "tag": "v1.0.2-12",
+    "raw": "v1.0.4-14-2-gb36b117-dirty",
+    "hash": "gb36b117",
+    "distance": 2,
+    "tag": "v1.0.4-14",
     "semver": {
         "options": {
             "loose": false,
             "includePrerelease": false
         },
         "loose": false,
-        "raw": "v1.0.2-12",
+        "raw": "v1.0.4-14",
         "major": 1,
         "minor": 0,
-        "patch": 2,
+        "patch": 4,
         "prerelease": [
-            12
+            14
         ],
         "build": [],
-        "version": "1.0.2-12"
+        "version": "1.0.4-14"
     },
-    "suffix": "7-g7353196-dirty",
-    "semverString": "1.0.2-12+7.g7353196",
-    "version": "1.0.4",
-    "build": 14
+    "suffix": "2-gb36b117-dirty",
+    "semverString": "1.0.4-14+2.gb36b117",
+    "version": "1.0.5",
+    "build": 15
 };
 /* tslint:enable */
