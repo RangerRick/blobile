452881c2ff8ee9ff95be6a2d3b2173b3c16f86c8
Benjamin Reed <ranger@opennms.org>
2020-09-09 12:42:09 -0400

    chore: bump to 1.1.4 build 24

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -2,29 +2,29 @@
 /* tslint:disable */
 export const VERSION = {
     "dirty": true,
-    "raw": "v1.1.2-22-6-g01d3bcd-dirty",
-    "hash": "g01d3bcd",
-    "distance": 6,
-    "tag": "v1.1.2-22",
+    "raw": "v1.1.3-23-0-gd70cfa1-dirty",
+    "hash": "gd70cfa1",
+    "distance": 0,
+    "tag": "v1.1.3-23",
     "semver": {
         "options": {
             "loose": false,
             "includePrerelease": false
         },
         "loose": false,
-        "raw": "v1.1.2-22",
+        "raw": "v1.1.3-23",
         "major": 1,
         "minor": 1,
-        "patch": 2,
+        "patch": 3,
         "prerelease": [
-            22
+            23
         ],
         "build": [],
-        "version": "1.1.2-22"
+        "version": "1.1.3-23"
     },
-    "suffix": "6-g01d3bcd-dirty",
-    "semverString": "1.1.2-22+6.g01d3bcd",
-    "version": "1.1.3",
-    "build": 23
+    "suffix": "0-gd70cfa1-dirty",
+    "semverString": "1.1.3-23",
+    "version": "1.1.4",
+    "build": 24
 };
 /* tslint:enable */
