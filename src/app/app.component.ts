39639af790b8692090e2dcf3076e47792c6a5399
Benjamin Reed <ranger@opennms.org>
2020-09-11 12:12:15 -0400

    feat: remember last URL for next launch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,8 @@ import { Plugins } from '@capacitor/core';
 const { SplashScreen } = Plugins;
 
 import { UpdateService } from '../lib/update.service';
+import { RouteWatcherService } from './route-watcher';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -16,12 +18,18 @@ export class AppComponent {
   constructor(
     private updateService: UpdateService,
     private platform: Platform,
+    private router: Router,
+    private routeWatcherService: RouteWatcherService,
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
     this.platform.ready().then(async () => {
+      const url = await this.routeWatcherService.lastUrl();
+      if (url) {
+        await this.router.navigateByUrl(url);
+      }
       SplashScreen.hide();
       return this.updateService.checkUpdate().finally(() => {
         return true;
