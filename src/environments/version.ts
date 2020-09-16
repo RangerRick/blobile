fd93f79bf569650dc4c6e5274e8532db1a195956
Benjamin Reed <ranger@opennms.org>
2020-09-16 10:24:56 -0400

    build: bump build, bump capacitor-eventsource dep

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -2,29 +2,29 @@
 /* tslint:disable */
 export const VERSION = {
     "dirty": true,
-    "raw": "v1.1.4-31-7-gc1050d5-dirty",
-    "hash": "gc1050d5",
-    "distance": 7,
-    "tag": "v1.1.4-31",
+    "raw": "v1.1.5-32-2-g5b02f6b-dirty",
+    "hash": "g5b02f6b",
+    "distance": 2,
+    "tag": "v1.1.5-32",
     "semver": {
         "options": {
             "loose": false,
             "includePrerelease": false
         },
         "loose": false,
-        "raw": "v1.1.4-31",
+        "raw": "v1.1.5-32",
         "major": 1,
         "minor": 1,
-        "patch": 4,
+        "patch": 5,
         "prerelease": [
-            31
+            32
         ],
         "build": [],
-        "version": "1.1.4-31"
+        "version": "1.1.5-32"
     },
-    "suffix": "7-gc1050d5-dirty",
-    "semverString": "1.1.4-31+7.gc1050d5",
+    "suffix": "2-g5b02f6b-dirty",
+    "semverString": "1.1.5-32+2.g5b02f6b",
     "version": "1.1.5",
-    "build": 32
+    "build": 33
 };
 /* tslint:enable */
