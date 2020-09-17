c91e2fd4c0c04ec9a59130ac909e6a4a022cb215
Benjamin Reed <ranger@opennms.org>
2020-09-16 21:47:21 -0400

    Blases Loaded 1.1.5 build 36

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -2,29 +2,29 @@
 /* tslint:disable */
 export const VERSION = {
     "dirty": true,
-    "raw": "v1.1.5-34-1-gf3dbb1a-dirty",
-    "hash": "gf3dbb1a",
+    "raw": "v1.1.5-35-1-g1acc05a-dirty",
+    "hash": "g1acc05a",
     "distance": 1,
-    "tag": "v1.1.5-34",
+    "tag": "v1.1.5-35",
     "semver": {
         "options": {
             "loose": false,
             "includePrerelease": false
         },
         "loose": false,
-        "raw": "v1.1.5-34",
+        "raw": "v1.1.5-35",
         "major": 1,
         "minor": 1,
         "patch": 5,
         "prerelease": [
-            34
+            35
         ],
         "build": [],
-        "version": "1.1.5-34"
+        "version": "1.1.5-35"
     },
-    "suffix": "1-gf3dbb1a-dirty",
-    "semverString": "1.1.5-34+1.gf3dbb1a",
+    "suffix": "1-g1acc05a-dirty",
+    "semverString": "1.1.5-35+1.g1acc05a",
     "version": "1.1.5",
-    "build": 35
+    "build": 36
 };
 /* tslint:enable */
