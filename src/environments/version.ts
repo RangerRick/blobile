35cb3a022ebbe030b92c200799b8463d12590469
Benjamin Reed <ranger@opennms.org>
2020-10-23 17:16:06 -0400

    release: bump version to 1.3.0

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -2,9 +2,9 @@
 /* tslint:disable */
 export const VERSION = {
     "dirty": true,
-    "raw": "v1.2.0-58-0-g6284f3e-dirty",
-    "hash": "g6284f3e",
-    "distance": 0,
+    "raw": "v1.2.0-58-21-g5f3244a-dirty",
+    "hash": "g5f3244a",
+    "distance": 21,
     "tag": "v1.2.0-58",
     "semver": {
         "options": {
@@ -22,9 +22,9 @@ export const VERSION = {
         "build": [],
         "version": "1.2.0-58"
     },
-    "suffix": "0-g6284f3e-dirty",
-    "semverString": "1.2.0-58",
-    "version": "1.2.0",
+    "suffix": "21-g5f3244a-dirty",
+    "semverString": "1.2.0-58+21.g5f3244a",
+    "version": "1.3.0",
     "build": 59
 };
 /* tslint:enable */
