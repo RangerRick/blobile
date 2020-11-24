30a7e056597d46f83fe54caf5f56f8ba107c9607
Benjamin Reed <ranger@opennms.org>
2020-11-24 11:44:52 -0500

    release: bump to build 69 (nice)

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -2,29 +2,29 @@
 /* tslint:disable */
 export const VERSION = {
     "dirty": true,
-    "raw": "v1.3.1-67-7-gcb034ea-dirty",
-    "hash": "gcb034ea",
-    "distance": 7,
-    "tag": "v1.3.1-67",
+    "raw": "v1.3.1-68-0-g4b8f3ec-dirty",
+    "hash": "g4b8f3ec",
+    "distance": 0,
+    "tag": "v1.3.1-68",
     "semver": {
         "options": {
             "loose": false,
             "includePrerelease": false
         },
         "loose": false,
-        "raw": "v1.3.1-67",
+        "raw": "v1.3.1-68",
         "major": 1,
         "minor": 3,
         "patch": 1,
         "prerelease": [
-            67
+            68
         ],
         "build": [],
-        "version": "1.3.1-67"
+        "version": "1.3.1-68"
     },
-    "suffix": "7-gcb034ea-dirty",
-    "semverString": "1.3.1-67+7.gcb034ea",
+    "suffix": "0-g4b8f3ec-dirty",
+    "semverString": "1.3.1-68",
     "version": "1.3.1",
-    "build": 68
+    "build": 69
 };
 /* tslint:enable */
