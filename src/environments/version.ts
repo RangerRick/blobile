17b7bbfcf317ddbf8e228f34809108d325e5d20b
Benjamin Reed <ranger@opennms.org>
2021-03-01 16:47:52 -0500

    release: 2.0.0 build 72
    * fix a race condition that could cause team info to not load
      (this would manifest in not having team names in the scorebox)

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -2,9 +2,9 @@
 /* tslint:disable */
 export const VERSION = {
     "dirty": true,
-    "raw": "v1.3.1-69-242-g5ed0a9c-dirty",
-    "hash": "g5ed0a9c",
-    "distance": 242,
+    "raw": "v1.3.1-69-245-ge39022f-dirty",
+    "hash": "ge39022f",
+    "distance": 245,
     "tag": "v1.3.1-69",
     "semver": {
         "options": {
@@ -22,8 +22,8 @@ export const VERSION = {
         "build": [],
         "version": "1.3.1-69"
     },
-    "suffix": "242-g5ed0a9c-dirty",
-    "semverString": "1.3.1-69+242.g5ed0a9c",
+    "suffix": "245-ge39022f-dirty",
+    "semverString": "1.3.1-69+245.ge39022f",
     "version": "2.0.0",
     "build": 72
 };
