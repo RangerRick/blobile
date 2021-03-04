01b492287371eb680f261a0e801dfe6f4c3792c2
Benjamin Reed <ranger@opennms.org>
2021-03-04 13:17:18 -0500

    release: bump to build 75

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -1,30 +1,30 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "dirty": false,
-    "raw": "v2.0.0-73-22-gc9e1645",
-    "hash": "gc9e1645",
-    "distance": 22,
-    "tag": "v2.0.0-73",
+    "dirty": true,
+    "raw": "v2.0.0-74-0-gc3e7bc5-dirty",
+    "hash": "gc3e7bc5",
+    "distance": 0,
+    "tag": "v2.0.0-74",
     "semver": {
         "options": {
             "loose": false,
             "includePrerelease": false
         },
         "loose": false,
-        "raw": "v2.0.0-73",
+        "raw": "v2.0.0-74",
         "major": 2,
         "minor": 0,
         "patch": 0,
         "prerelease": [
-            73
+            74
         ],
         "build": [],
-        "version": "2.0.0-73"
+        "version": "2.0.0-74"
     },
-    "suffix": "22-gc9e1645",
-    "semverString": "2.0.0-73+22.gc9e1645",
+    "suffix": "0-gc3e7bc5-dirty",
+    "semverString": "2.0.0-74",
     "version": "2.0.0",
-    "build": 74
+    "build": 75
 };
 /* tslint:enable */
